a0446e7c58204d74fa0068f32b233b6e04e4d0dc
Carlyn Savino <44906591+csav3@users.noreply.github.com>
2020-05-29 12:50:27 -0400

    Added code for saving to csv

--- training.py
+++ training.py
@@ -7,44 +7,60 @@ from keras.layers import MaxPooling2D, Conv2D
 from itertools import combinations
 from itertools import permutations
 import random
+import math
 
 import numpy as np
 import os
 import pandas
 
-# trainingset = pandas.read_pickle('trainingset_100_images_each_class.pkl') 
+# function for rounding down; used to get number of samples
+def round_down(n, decimals=0):
+    multiplier = 10 ** decimals
+    return math.floor(n * multiplier) / multiplier
 
+#reads in dataset; path may differ on your own machine
+trainingset = pandas.read_pickle('trainingset_100_images_each_class.pkl') 
 
 classes = []
-# Adds class names from each sample to a list (includes duplicates)
+
+# adds class names from each sample to a list (includes duplicates)
 for i in range(len(trainingset.index)):
     classes.append(trainingset.iloc[i,5])
 
-# Removes duplicate class names
+# removes duplicate class names
 classes = np.unique(classes) 
 
 # create a variable for number of classes used
 num_classes = 3
 
-# All possible combinations of 3 classes
+# all possible combinations of 3 classes
 list_combos = list(combinations(classes, num_classes)) # order matters; no repeated elements
 
-# randomly select 1 for now
 # create a variable for number of combos
-num_combos = 1
+num_combos = 2
 
 combos_random = random.sample(list_combos, num_combos)
+
+# clears the csv file for new program run-through
+if os.path.exists('metrics.csv'):
+    os.remove('metrics.csv')
+
+# creates pandas dataframe with column names
+results = pandas.DataFrame(columns=['num_combos', 'num_layers', 'concat_axis', 'classes', 'num_samples', 'num_val_samples', 'accuracy', 'val_accuracy'])
 
 for i in range(1, 4):
     tset_labels = []
     tset_data = []
     for combos in combos_random:
+        classes_used = []
         for index, c in enumerate(combos):
             rows_match = trainingset.loc[trainingset['true_label'] == c]
             half_sec_img = np.vstack(rows_match.iloc[:,0].values)
             one_sec_img = np.vstack(rows_match.iloc[:,1].values)
             two_sec_img = np.vstack(rows_match.iloc[:,2].values)
             three_sec_img = np.vstack(rows_match.iloc[:,3].values)
+
+            classes_used.append(c)
 
             half_sec_img_reshape = half_sec_img.reshape(-1, 140, 170,1)
             one_sec_img_reshape = one_sec_img.reshape(-1, 140, 170,1)
@@ -53,11 +69,13 @@ for i in range(1, 4):
 
             tset_labels.extend([index] * len(rows_match)) #extend keeps a single list and extends it
             tset_data.append(np.concatenate((half_sec_img_reshape, one_sec_img_reshape, two_sec_img_reshape, three_sec_img_reshape), axis = i))
+
         tset_data_array = np.vstack(tset_data)
         print(tset_data_array.shape)
         tset_labels_array = np.asarray(tset_labels)
-        # print(tset_labels_array.shape)
-        
+
+        classes_used = np.array(classes_used)
+
         model = Sequential()
 
         model.add(Conv2D(16, (3,3), input_shape= tset_data_array.shape[1:4])) 
@@ -86,23 +104,34 @@ for i in range(1, 4):
 
         
         model.summary()
-    # if 3 layers is true, call function that does only 3 layers.
-    # if not, do 4 layers
 
         model.compile(loss = 'sparse_categorical_crossentropy',
                     optimizer='adadelta',
                     metrics=['accuracy'])
      
 
-    #fit using the training data and training labels
+        # portion of data used for validation
+        val_amount = 0.15
 
-        fit = model.fit(tset_data_array, tset_labels_array, validation_split = 0.15, epochs = 1)
-    
-# Print table information at the end of each loop 
-    # save the results to something - look up how to do this; save training results to file
-    # A table with: Number of layers: num_layers
-                    # Classes: num_classes
-                    # Number of samples: num_samples
-                    # Training score: train_acc
-                    # Validation score: val_acc
+        # fit using the training data and training labels
+        fit = model.fit(tset_data_array, tset_labels_array, validation_split = val_amount, epochs = 1)
 
+        # obtain number of samples
+        num_samples = round_down((1 - val_amount) * len(tset_data_array))
+
+        # obtain number of layers
+        num_layers = len(model.layers) 
+
+        # fill in pandas dataframe
+        results = results.append({'num_combos': num_combos,
+                                    'num_layers': num_layers,
+                                    'concat_axis': i,
+                                    'classes': classes_used,
+                                    'num_samples': num_samples,
+                                    'num_val_samples': len(tset_data_array) - num_samples,
+                                    'accuracy': fit.history['accuracy'],
+                                    'val_accuracy': fit.history['val_accuracy']
+                                    }, ignore_index=True)
+
+# save results to csv file
+results.to_csv('metrics.csv', mode = 'a', header = True)
